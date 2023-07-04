cd2d2d3bcf52699f113d065a689841901363223e
Naveen <135813797+Naveencodespeaks@users.noreply.github.com>
2023-07-04 21:11:22 +0530

    MySQL Basics
    MySQL Basics notes

---
+++ MYSQL/Not_Operator.sql
@@ -0,0 +1,23 @@
+# Not Operator
+
+
+use naveendb1;
+
+desc employee_6;
+
+select * from employee_6;
+
+# Not operations
+select * from employee_6
+where not EmpAge = 27;
+
+# Not operations
+
+select * from employee_6
+where not FirstName = 'Tom';
+
+
+# Not operations
+
+select * from employee_6
+where not LastName = 'Stone';
