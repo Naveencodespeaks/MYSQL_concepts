b9e6e48548ab489f330b7e9620c67d421a3b60fc
Naveen <135813797+Naveencodespeaks@users.noreply.github.com>
2023-07-04 21:22:02 +0530

    MYSQL BASICS
    MYSQL BASIC CONCEPTS

---
+++ MYSQL/comments_single-line and Multi _line.sql
@@ -0,0 +1,25 @@
+# comment single-line and Multi _line
+
+use naveendb1;
+show databases;
+
+desc employe2;
+
+
+# single line comment
+select * from employee; # fetch all the recods from the table
+
+
+-- singleline comment
+select * from employee; -- to fetch all the records from a table
+
+/* multiline comment *\
+
+
+/* multi line comment
+Retrivr all the records from
+table Employee. we are using select
+statement
+*/
+
+
